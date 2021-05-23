ebe27fa9f6b4aa2191a330b6c3dd0de16d75e092
Ryota Ikezawa <pavegy@gmail.com>
2021-05-24 01:40:17 +0900

    :recycle: Update aria-label

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,7 +4,7 @@ export const Header: React.VFC = (): JSX.Element => {
     return (
         <header className="body-font bg-gray-900">
             <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-                <a className="flex title-font font-medium items-center mb-4 md:mb-0">
+                <a className="flex title-font font-medium items-center mb-4 md:mb-0" href={'/'}>
                     <span className="text-xl text-white">Funai Gaming</span>
                 </a>
                 <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
