c37c89d8ee2edb79a24afb084e3470d57c68eb1b
Ryota Ikezawa <pavegy@gmail.com>
2021-05-25 04:59:33 +0900

    :recycle: Update storybook

--- src/components/parts/buttons/iconButton.stories.tsx
+++ src/components/parts/buttons/iconButton.stories.tsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import { Story, Meta } from '@storybook/react'
+import TwitterIcon from '@material-ui/icons/Twitter'
 
 import { IconButton, IconButtonProps } from './iconButton'
 
 export default {
-  title: 'Example/Button',
+  title: 'Example/IconButton',
   component: IconButton,
 } as Meta
 
 const Template: Story<IconButtonProps> = (args) => {
-  return <IconButton {...args}>Button</IconButton>
+  return (
+    <IconButton IconComponent={TwitterIcon} {...args}>
+      IconButton
+    </IconButton>
+  )
 }
 
 export const Button = Template.bind({})
